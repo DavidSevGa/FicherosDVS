200db3dd44b446ce7be384ec48406763c6ad1405
David <david-s-g@hotmail.es>
2021-01-17 18:42:09 +0100

    subiendo mi primer proyecto

---
+++ app/src/main/java/hfad/com/proyectoficherosandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package hfad.com.proyectoficherosandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String translate = getResources().getString(R.string.title);
+    }
+}
